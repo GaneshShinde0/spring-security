6a82427dba9f0080a2d6f1b90f0b8062e2a3e798
GaneshShinde0 <ganeshshinde1868@gmail.com>
2026-03-08 20:55:51 +0530

    BCryptPasswordEncoderChanges

---
+++ src/main/java/com/ganesh/SpringSecurity/service/UserService.java
@@ -0,0 +1,20 @@
+package com.ganesh.SpringSecurity.service;
+
+import com.ganesh.SpringSecurity.model.Users;
+import com.ganesh.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepo.save(user);
+    }
+}
